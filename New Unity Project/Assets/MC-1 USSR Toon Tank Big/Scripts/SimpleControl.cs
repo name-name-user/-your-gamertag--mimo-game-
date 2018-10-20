9028dc64d2e683da41fff00c376555d3c6c92d40
name-name-user <luke.is.southard@gmail.com>
2018-10-19 20:11:46 -0400

    c

--- New Unity Project/Assets/MC-1 USSR Toon Tank Big/Scripts/SimpleControl.cs
+++ New Unity Project/Assets/MC-1 USSR Toon Tank Big/Scripts/SimpleControl.cs
@@ -1,118 +1,142 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SimpleControl : MonoBehaviour {
-	
-	//all left wheels
-    public GameObject[] LeftWheels;
-	//all right wheels
-    public GameObject[] RightWheels;
+public class SimpleControl : MonoBehaviour
+{
 
-    public GameObject LeftTrack;
+    //all left wheels
+    /* public GameObject[] LeftWheels;
+     //all right wheels
+     public GameObject[] RightWheels;
 
-    public GameObject RightTrack;
+     public GameObject LeftTrack;
 
-    public float wheelsSpeed = 2f;
-    public float tracksSpeed = 2f;
-    public float forwardSpeed = 1f;
-    public float rotateSpeed = 1f;
+     public GameObject RightTrack;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//Keyboard moves =======================================//
-        //Forward Move
-        if (Input.GetKey(KeyCode.W))
-        {
-            //Left wheels rotate
-            foreach (GameObject wheelL in LeftWheels)
-            {
-                wheelL.transform.Rotate(new Vector3(wheelsSpeed, 0f, 0f));
-            }
-            //Right wheels rotate
-            foreach (GameObject wheelR in RightWheels)
-            {
-                wheelR.transform.Rotate(new Vector3(-wheelsSpeed, 0f, 0f));
-            }
-            //left track texture offset
-            LeftTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f,Time.deltaTime*tracksSpeed);
-            //right track texture offset
-            RightTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * tracksSpeed);
+     public float wheelsSpeed = 2f;
+     public float tracksSpeed = 2f;
+     public float forwardSpeed = 1f;
+     public float rotateSpeed = 1f;
 
-            //Move Tank
+     // Use this for initialization
+     void Start () {
 
-            transform.Translate(new Vector3(0f, 0f, forwardSpeed));
+     }
 
-        }
-        //Back Move
-        if (Input.GetKey(KeyCode.S))
-        {
-            //Left wheels rotate
-            foreach (GameObject wheelL in LeftWheels)
-            {
-                wheelL.transform.Rotate(new Vector3(-wheelsSpeed, 0f, 0f));
-            }
-            //Right wheels rotate
-            foreach (GameObject wheelR in RightWheels)
-            {
-                wheelR.transform.Rotate(new Vector3(wheelsSpeed, 0f, 0f));
-            }
-            //left track texture offset
-            LeftTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * -tracksSpeed);
-            //right track texture offset
-            RightTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * -tracksSpeed);
-            //Move Tank
-            transform.Translate(new Vector3(0f, 0f, -forwardSpeed));
-        }
-        //On Left
-        if (Input.GetKey(KeyCode.A))
-        {
-            //Left wheels rotate
-            foreach (GameObject wheelL in LeftWheels)
-            {
-                wheelL.transform.Rotate(new Vector3(wheelsSpeed, 0f, 0f));
-            }
-            //Right wheels rotate
-            foreach (GameObject wheelR in RightWheels)
-            {
-                wheelR.transform.Rotate(new Vector3(wheelsSpeed, 0f, 0f));
-            }
-            //left track texture offset
-            LeftTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * tracksSpeed);
-            //right track texture offset
-            RightTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * -tracksSpeed);
-            //Rotate Tank
-            transform.Rotate(new Vector3(0f,-rotateSpeed,0f));
-        }
-        //On Right
-        if (Input.GetKey(KeyCode.D))
-        {
-            //Left wheels rotate
-            foreach (GameObject wheelL in LeftWheels)
-            {
-                wheelL.transform.Rotate(new Vector3(-wheelsSpeed, 0f, 0f));
-            }
-            //Right wheels rotate
-            foreach (GameObject wheelR in RightWheels)
-            {
-                wheelR.transform.Rotate(new Vector3(-wheelsSpeed, 0f, 0f));
-            }
-            //left track texture offset
-            LeftTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * -tracksSpeed);
-            //right track texture offset
-            RightTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * tracksSpeed);
-            //Rotate Tank
-            transform.Rotate(new Vector3(0f, rotateSpeed, 0f));
-        }
-		//=======================================//
-                                                   
+     // Update is called once per frame
+     void Update () {
+         //Keyboard moves =======================================//
+         //Forward Move
+         if (Input.GetKey(KeyCode.W))
+         {
+             //Left wheels rotate
+             foreach (GameObject wheelL in LeftWheels)
+             {
+                 wheelL.transform.Rotate(new Vector3(wheelsSpeed, 0f, 0f));
+             }
+             //Right wheels rotate
+             foreach (GameObject wheelR in RightWheels)
+             {
+                 wheelR.transform.Rotate(new Vector3(-wheelsSpeed, 0f, 0f));
+             }
+             //left track texture offset
+             LeftTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f,Time.deltaTime*tracksSpeed);
+             //right track texture offset
+             RightTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * tracksSpeed);
 
+             //Move Tank
+
+             transform.Translate(new Vector3(0f, 0f, forwardSpeed));
+
+         }
+         //Back Move
+         if (Input.GetKey(KeyCode.S))
+         {
+             //Left wheels rotate
+             foreach (GameObject wheelL in LeftWheels)
+             {
+                 wheelL.transform.Rotate(new Vector3(-wheelsSpeed, 0f, 0f));
+             }
+             //Right wheels rotate
+             foreach (GameObject wheelR in RightWheels)
+             {
+                 wheelR.transform.Rotate(new Vector3(wheelsSpeed, 0f, 0f));
+             }
+             //left track texture offset
+             LeftTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * -tracksSpeed);
+             //right track texture offset
+             RightTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * -tracksSpeed);
+             //Move Tank
+             transform.Translate(new Vector3(0f, 0f, -forwardSpeed));
+         }
+         //On Left
+         if (Input.GetKey(KeyCode.A))
+         {
+             //Left wheels rotate
+             foreach (GameObject wheelL in LeftWheels)
+             {
+                 wheelL.transform.Rotate(new Vector3(wheelsSpeed, 0f, 0f));
+             }
+             //Right wheels rotate
+             foreach (GameObject wheelR in RightWheels)
+             {
+                 wheelR.transform.Rotate(new Vector3(wheelsSpeed, 0f, 0f));
+             }
+             //left track texture offset
+             LeftTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * tracksSpeed);
+             //right track texture offset
+             RightTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * -tracksSpeed);
+             //Rotate Tank
+             transform.Rotate(new Vector3(0f,-rotateSpeed,0f));
+         }
+         //On Right
+         if (Input.GetKey(KeyCode.D))
+         {
+             //Left wheels rotate
+             foreach (GameObject wheelL in LeftWheels)
+             {
+                 wheelL.transform.Rotate(new Vector3(-wheelsSpeed, 0f, 0f));
+             }
+             //Right wheels rotate
+             foreach (GameObject wheelR in RightWheels)
+             {
+                 wheelR.transform.Rotate(new Vector3(-wheelsSpeed, 0f, 0f));
+             }
+             //left track texture offset
+             LeftTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * -tracksSpeed);
+             //right track texture offset
+             RightTrack.transform.GetComponent<Renderer>().material.mainTextureOffset += new Vector2(0f, Time.deltaTime * tracksSpeed);
+             //Rotate Tank
+             transform.Rotate(new Vector3(0f, rotateSpeed, 0f));
+         }*/
+    //=======================================//
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        winText.text = "";
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddRelativeForce(movement * speed);
 
 
     }
 }
+       
+
